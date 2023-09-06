8d531f0ce13ca079aea6ac13f98d85e1d1d2903e
Victor James <victorjamesa47@gmail.com>
2023-09-06 10:18:19 +0100

    Commit for review

--- Location_allocation.R
+++ Location_allocation.R
@@ -1,26 +1,32 @@
-libs <- c('viridis', 'sf','stars','tidyverse','ggthemes','rvest','ggrepel','plotly','raster','terra')
+libs <- c('viridis', 'sf','stars','tidyverse','ggthemes','rvest','ggrepel','plotly','raster','terra','ggplot2')
 invisible(
   lapply(libs,library,character.only = T)
 )
 
-library(ggplot2)
 
 
-
-
+#load raster and shapefiles
 ras_pop <- read_stars("/Users/User/Documents/GIS/Healthcare Allocation Analysis/raster/mubi.tif")
 mubi_wards <- read_sf ("/Users/User/Documents/GIS/Healthcare Allocation Analysis/nigeria_health_facilities/Wards_mubi.shp")
+health_f <- read_sf ("/Users/User/Documents/GIS/Healthcare Allocation Analysis/nigeria_health_facilities/Nigeria_-_Health_Care_Facilities_.shp") 
 
+#set uniform coordinate reference system for both datasets
 mubi_wards <- st_transform(mubi_wards,crs = st_crs(ras_pop))
 
-# Extract zonal statistics 
+# Extract zonal statistics to get population sum for each ward
 zonal_stats <- zonal(ras_pop, mubi_wards, sum)
 
-ras_pop <- st_as_stars(ras_pop)
+#get a subset of health facilites in mubi wards 
+mubi_health <- st_intersection(health_f,mubi_wards)
+
+#calculate euclidean distances between health facilities in mubi and convert to raster
+mubi_euclidist <- st_distance(mubi_health)
+mubi_euclidist <- raster::raster(mubi_euclidist)
+
+
 
 ggplot() +
   geom_stars(data = ras_pop,na.rm=TRUE,alpha=1) + 
   scale_fill_viridis_c(na.value = "white") +
   theme_void()
-  
   
